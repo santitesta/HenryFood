97f69f98f6c950248432523d69448205f8e7d0a4
Midasisking <testasanty@gmail.com>
2022-04-15 12:41:33 -0300

    DB SetUp

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -7,6 +7,10 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
+router.get('/recipe', async (req,res) =>{
+  let recipe = (await fetch('https://rickandmortyapi.com/api/character')).info.count
+  console.log(recipe)
+  res.send('Bro')
+})
 
 module.exports = router;
