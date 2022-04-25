3b3b2effc1bb401d74c4f8d0008fb2f86f54ecd8
Midasisking <testasanty@gmail.com>
2022-04-24 22:31:23 -0300

    Dunno

--- api/src/routes/Diet.js
+++ api/src/routes/Diet.js
@@ -7,3 +7,7 @@ router.get('/', getDiets)
 router.post('/', createDiet)
 
 module.exports = router;
+
+// [ ] GET /types:
+// Obtener todos los tipos de dieta posibles
+// En una primera instancia, cuando no exista ninguno, deberán precargar la base de datos con los tipos de datos indicados por spoonacular acá
--- api/src/routes/Recipe.js
+++ api/src/routes/Recipe.js
@@ -9,3 +9,14 @@ router.get('/:id', getRecipeById)
 router.post('/', createRecipe)
 
 module.exports = router;
+
+// [ ] GET /recipes?name="...":
+// Obtener un listado de las recetas que contengan la palabra ingresada como query parameter
+// Si no existe ninguna receta mostrar un mensaje adecuado
+// [ ] GET /recipes/{idReceta}:
+// Obtener el detalle de una receta en particular
+// Debe traer solo los datos pedidos en la ruta de detalle de receta
+// Incluir los tipos de dieta asociados
+// [ ] POST /recipe:
+// Recibe los datos recolectados desde el formulario controlado de la ruta de creación de recetas por body
+// Crea una receta en la base de datos
