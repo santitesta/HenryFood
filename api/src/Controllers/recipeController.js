d13fcb909ea5af9581201c96a3f34d70e8431668
Midasisking <testasanty@gmail.com>
2022-04-29 20:32:53 -0300

    Hardcoding double apiKey

--- api/src/Controllers/recipeController.js
+++ api/src/Controllers/recipeController.js
@@ -6,24 +6,24 @@ const { apiKey } = process.env;
 const URLid = "https://api.spoonacular.com/recipes/"
 const URLcs = `https://api.spoonacular.com/recipes/complexSearch?addRecipeInformation=true&apiKey=${apiKey}`
 
-const fs = require('fs')
+const fs = require('fs');
 let jsonData = JSON.parse(fs.readFileSync('spoonacular.json', 'utf-8'))
 
 async function getRecipe(req,res){
   try {
-    // let query = req.query.query
-    // let recipes = (await axios.get(`${URLcs}&query=${query}&number=90`)).data.results
-    // .map(e => {
-    //   return {
-    //     id: e.id,
-    //     title: e.title,
-    //     summary: e.summary,
-    //     points: e.spoonacularScore,
-    //     image: e.image
-    //   }
-    // })
-
-    let recipes = jsonData.results
+    let query = req.query.query
+    let recipes = (await axios.get(`${URLcs}&query=${query}&number=90`)).data.results
+    .map(e => {
+      return {
+        id: e.id,
+        title: e.title,
+        summary: e.summary,
+        points: e.spoonacularScore,
+        image: e.image
+      }
+    })
+    
+    // let recipes = jsonData.results
 
     let allRecipes = (await Recipes.findAll()).concat(recipes)
 
@@ -34,23 +34,31 @@ async function getRecipe(req,res){
 }
 
 async function getRecipeById(req,res){
+  // Traer la info desde la base de datos tambien!!!
   const {id} = req.params;
-  await axios.get(`${URLid}${id}/information?apiKey=${apiKey}`)
-    .then(response => {
-      let recipe = {
-        id: response.data.id,
-        name: response.data.title,
-        summary: response.data.summary,
-        points: response.data.spoonacularScore,
-        healthScore: response.data.healthScore,
-        image: response.data.image,
-        diets: response.data.diets,
-        dishTypes: response.data.dishTypes,
-        analyzedInstructions: response.data.analyzedInstructions,
-      }
-      res.send(recipe)
-    })
-    .catch(error => console.log(error))
+  let recipe = Recipes.findOne({
+    where: {
+      id: id
+    }
+  })
+    .then(console.log('TODO FLAMA', recipe))
+    .catch(error => console.log('todo mal'))
+  // await axios.get(`${URLid}${id}/information?apiKey=${apiKey}`)
+  //   .then(response => {
+  //     let recipe = {
+  //       id: response.data.id,
+  //       name: response.data.title,
+  //       summary: response.data.summary,
+  //       points: response.data.spoonacularScore,
+  //       healthScore: response.data.healthScore,
+  //       image: response.data.image,
+  //       diets: response.data.diets,
+  //       dishTypes: response.data.dishTypes,
+  //       analyzedInstructions: response.data.analyzedInstructions,
+  //     }
+  //     res.send(recipe)
+  //   })
+  //   .catch(error => console.log(error))
 }
 
 async function createRecipe(req,res){
