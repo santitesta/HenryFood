eccbf607f9d058a15cef028a5017d79fd5fab5e6
Midasisking <testasanty@gmail.com>
2022-05-11 16:44:26 -0300

    Feat(Validation - Name cannot have special char)

--- client/src/Misc/validate.js
+++ client/src/Misc/validate.js
@@ -4,6 +4,8 @@ export const validate = (input) => {
   let errors = {}
 
   if(!input.name) errors.name = 'Must have a name'
+  let pattern = /^[a-zA-Z0-9]*$/
+  if(!pattern.test(input.name)) errors.name = 'Cannot contain special characters'
 
   if(Number(input.points) < 0 || Number(input.points) > 100) {
     errors.points = 'Must be a number between 0 and 100'
